ce5ded66634c9fe84af6b3b14ca66dfbe9733959
Collin Johnson <collin.johnson@e-hps.com>
2017-12-03 18:13:00 -0600

    Added release, made the game start larger

--- LD40-2/Game1.cs
+++ LD40-2/Game1.cs
@@ -90,6 +90,8 @@ namespace LD40_2
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.PreferredBackBufferWidth = 1280;
             Content.RootDirectory = "Content";
         }
 
